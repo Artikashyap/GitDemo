9f499f93730904009603bac752b55d6045b84fcb
Artikashyap <kashyapprinci1231@gmail.com>
2025-09-15 17:12:45 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,48 @@
+# Takes two numbers as input from the user.
+# Performs the basic mathematical operations on these two numbers:
+#	Addition
+#	Subtraction
+#	Multiplication
+#	Division
+
+
+
+a = int(input("Enter a number : "))
+b = int(input("Enter a number : " ))
+sum = (a + b)
+print( "sum : ",sum)
+
+
+
+a = int(input("Enter a number : "))
+b = int(input("Enter a number : " ))
+sub = (a - b)
+print( "sub : ",sub)
+
+
+a = int(input("Enter a number : "))
+b = int(input("Enter a number : " ))
+multi = (a * b)
+print( "multi : ",multi)
+
+
+
+a = int(input("Enter a number : "))
+b = int(input("Enter a number : " ))
+div = (a / b)
+print( "div : ",div)
+
+
+
+
+
+# 1.  Takes a user's first name and last name as input.
+# 2.  Concatenates the first name and last name into a full name.
+# 3.  Prints a personalized greeting message using the full name.
+
+first = input("enter your first name : ")
+last = input("enter your last name : ")
+full=(first + " " + last)
+print("Hello ,", full, "welcome to the python program ")
+
+
