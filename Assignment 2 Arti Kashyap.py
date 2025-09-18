049175e46272fafab62adeba25a5e1b4e621f9eb
Artikashyap <kashyapprinci1231@gmail.com>
2025-09-18 18:19:03 +0530

    Add files via upload

---
+++ Assignment 2 Arti Kashyap.py
@@ -0,0 +1,30 @@
+""""
+1. 	Takes an integer input from the user.
+2. 	Checks whether the number is even or odd using an if-else statement.
+3. 	Displays the result accordingly.
+"""
+
+num=int(input("enter a number: "))
+if num % 2== 0:
+       print(num, "is an even number.")
+else:
+       print(num, "is an odd number.")
+       
+
+"""
+1.   Uses a for loop to iterate over numbers from 1 to 50.
+2.   Calculates the sum of all integers in this range.
+3.   Displays the final sum.
+"""
+
+
+
+
+
+total = 0
+
+for i in range(1, 51):
+    total += i
+
+print("The sum of numbers from 1 to 50 is:", total)
+
